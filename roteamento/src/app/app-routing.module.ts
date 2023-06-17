4fedf68e11013a46405b289ca4d7f692232655af
Michel Braga <31113222+mbraga2023@users.noreply.github.com>
2023-06-17 11:16:27 -0400

    aula fundamentos angular

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 const routes: Routes = [
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent}
+  {path: "primeira-pagina", component: PrimeiraPaginaComponent},
+  {path: "segunda-pagina", component: SegundaPaginaComponent},
+  {path: "", redirectTo:"primeira-pagina", pathMatch: "full"},
+  {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+
+  {path: "**", component: PaginaNaoEncontradaComponent },
+
 ]
 
 @NgModule({
