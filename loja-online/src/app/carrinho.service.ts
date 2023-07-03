84ad7fe9b264a9c5e91ed3f185e9d205c7ffdb30
Michel Braga <31113222+mbraga2023@users.noreply.github.com>
2023-07-02 23:00:11 -0300

    logica carrinho de compras

--- loja-online/src/app/carrinho.service.ts
+++ loja-online/src/app/carrinho.service.ts
@@ -10,12 +10,19 @@ export class CarrinhoService {
   constructor() { }
 
   obtemCarrinho(){
-    return JSON.parse(localStorage.getItem("carrinho") || "");
-    
+    this.itens = JSON.parse(localStorage.getItem("carrinho") || "[]");
+    console.log(this.itens);
+    return this.itens;
   }
 
   adicionarAoCarrinho (produto: IProdutoCarrinho){
     this.itens.push(produto);
+    localStorage.setItem("carrinho",JSON.stringify(this.itens));
+
+  }
+
+  removerProdutoCarrinho(produtoId: number){
+    this.itens = this.itens.filter(item => item.id !== produtoId);
     localStorage.setItem("carrinho",JSON.stringify(this.itens));
 
   }
