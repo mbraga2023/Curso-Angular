cfc2ca5d6543ff87634a9b702701b768defd0e21
Michel Braga <31113222+mbraga2023@users.noreply.github.com>
2023-07-07 16:10:16 -0300

    implementacao buscador

--- loja-online/src/app/produtos/produtos.component.ts
+++ loja-online/src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduto } from '../produtos';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -11,10 +12,21 @@ export class ProdutosComponent implements OnInit{
   produtos: IProduto [] | undefined;
 
   constructor (
-    private produtosService: ProdutosService
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
-    this.produtos = this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLocaleLowerCase();
+      
+
+      if (descricao){
+        this.produtos = produtos.filter(produto => produto.descricao.toLocaleLowerCase().includes(descricao) );
+        return;
+      }
+      this.produtos = produtos;
+    });
   }
 }
